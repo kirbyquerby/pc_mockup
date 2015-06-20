27dd1e561dcdb8fe7890199e80e54c9ca9c03e8b
sanjayyepuri <sanjay.yepuri@gmail.com>
2015-06-20 12:40:14 -0500

    Commit #4

--- routes/competition.js
+++ routes/competition.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var dashboard = require('./dashboard');
+router.use('/dashboard', dashboard);
 router.get('/info', function(req, res){
     res.render('info',{title: "Info"});
 });
